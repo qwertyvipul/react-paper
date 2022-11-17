a26b2d280db671ef9a60d2361269c26c0bd891cd
Vipul Sharma <vipuls526@gmail.com>
2022-11-18 03:12:13 +0530

    First publish

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+    stories: ['../stories/**/*.stories.mdx', '../src/**/*.stories.tsx'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    framework: '@storybook/react',
+};
---
+++ lib/components/Button/Button.stories.d.ts
@@ -0,0 +1,4 @@
+import { Meta } from "@storybook/react/types-7-0";
+declare const _default: Meta<import("@storybook/csf").Args>;
+export default _default;
+export declare const PrimaryButton: () => JSX.Element;
